45f0224953125591aa799ffb1b914ba7aeed2a2d
Eidan Khan <76518500+eidankhan@users.noreply.github.com>
2026-01-11 01:52:00 +0000

    update config.js

--- frontend/config.js
+++ frontend/config.js
@@ -2,10 +2,9 @@
 
 /**
  * Global configuration for the application.
- * Change window.APP_CONFIG.API_BASE_URL to your production server URL when deploying.
+ * Change API_BASE_URL to your production server URL when deploying.
  */
-// const window.APP_CONFIG.API_BASE_URL = 'http://127.0.0.1:8000';
 
 window.APP_CONFIG = {
-    window.APP_CONFIG.API_BASE_URL: "/api"
+    API_BASE_URL: "/api"
 };
