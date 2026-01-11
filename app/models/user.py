b6d5ef8df40ff0cfad730750958fa4626502210d
Eidan Khan <76518500+eidankhan@users.noreply.github.com>
2026-01-11 05:57:01 +0000

    added a new colum is_superuser

--- app/models/user.py
+++ app/models/user.py
@@ -11,3 +11,5 @@ class User(Base):
     password_hash = Column(String(255), nullable=False)
     is_verified = Column(Boolean, nullable=False, default=False)
     created_at = Column(DateTime(timezone=True), server_default=func.now())
+    is_superuser = Column(Boolean, nullable=False, default=False)
+
